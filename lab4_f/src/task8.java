e9933e129d6226148ed448f62ace60faf284c17d
pelmach <68530775+pelmach@users.noreply.github.com>
2022-03-22 11:55:21 +0500

    Add files via upload

--- lab4_f/src/task8.java
+++ lab4_f/src/task8.java
@@ -21,58 +21,58 @@ public class task8 {
     }
     static void Matrix(int[][] matrix) {
         int k = 1;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < matrix.length/2 - 1; i++) {
             matrix[i][0] = k;
             k++;
         }
-        for (int i = 3; i > 0; i--) {
+        for (int i = matrix.length/2 - 1; i > 0; i--) {
             matrix[i][matrix.length/2 - i - 1] = k;
             k++;
         }
-        for (int i = 3; i > 0; i--) {
+        for (int i = matrix.length/2 - 1; i > 0; i--) {
             matrix[0][i] = k;
             k++;
         }
 
         k = 1;
         for (int i = 0; i < matrix.length/2 - 1; i++) {
-            matrix[i][4] = k;
+            matrix[i][matrix.length/2] = k;
             k++;
         }
-        for (int i = 3; i > 0; i--) {
+        for (int i = matrix.length/2 - 1; i > 0; i--) {
             matrix[i][matrix.length - i - 1] = k;
             k++;
         }
-        for (int i = matrix.length - 1; i > 4; i--) {
+        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
             matrix[0][i] = k;
             k++;
         }
 
         k = 1;
-        for (int i = 4; i < matrix.length - 1; i++) {
-            matrix[i][4] = k;
+        for (int i = matrix.length/2; i < matrix.length - 1; i++) {
+            matrix[i][matrix.length/2] = k;
             k++;
         }
-        for (int i = 7; i > 4; i--) {
-            matrix[i][4 + matrix.length - i - 1] = k;
+        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
+            matrix[i][matrix.length/2 + matrix.length - i - 1] = k;
             k++;
         }
-        for (int i = matrix.length - 1; i > 4; i--) {
-            matrix[4][i] = k;
+        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
+            matrix[matrix.length/2][i] = k;
             k++;
         }
 
         k = 1;
-        for (int i = 4; i < matrix.length - 1; i++) {
+        for (int i = matrix.length/2; i < matrix.length - 1; i++) {
             matrix[i][0] = k;
             k++;
         }
-        for (int i = 7; i > 4; i--) {
+        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
             matrix[i][matrix.length - i - 1] = k;
             k++;
         }
-        for (int i = 3; i > 0; i--) {
-            matrix[4][i] = k;
+        for (int i = matrix.length/2 - 1; i > 0; i--) {
+            matrix[matrix.length/2][i] = k;
             k++;
         }
 
