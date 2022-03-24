60562112a65d5eb46d33edfe5e18fca97c609d59
pelmach <68530775+pelmach@users.noreply.github.com>
2022-03-24 23:32:01 +0500

    Add files via upload

--- lab4_f/src/task8.java
+++ lab4_f/src/task8.java
@@ -21,61 +21,86 @@ public class task8 {
     }
     static void Matrix(int[][] matrix) {
         int k = 1;
-        for (int i = 0; i < matrix.length/2 - 1; i++) {
+        for (int i = 0; i < matrix.length / 2 - 1; i++) {
             matrix[i][0] = k;
             k++;
         }
-        for (int i = matrix.length/2 - 1; i > 0; i--) {
-            matrix[i][matrix.length/2 - i - 1] = k;
+        for (int i = matrix.length / 2 - 1; i > 0; i--) {
+            matrix[i][matrix.length / 2 - i - 1] = k;
             k++;
         }
-        for (int i = matrix.length/2 - 1; i > 0; i--) {
+        for (int i = matrix.length / 2 - 1; i > 0; i--) {
             matrix[0][i] = k;
             k++;
         }
+        for (int i = 0; i < matrix.length/2 - 2; i++) {
+            for (int j = 0; j < matrix.length/2 - 2; j++) {
+                if(matrix[i][j] == 0)
+                    matrix[i][j] = k;
+            }
+        }
+
 
         k = 1;
-        for (int i = 0; i < matrix.length/2 - 1; i++) {
-            matrix[i][matrix.length/2] = k;
+        for (int i = 0; i < matrix.length / 2 - 1; i++) {
+            matrix[i][matrix.length / 2] = k;
             k++;
         }
-        for (int i = matrix.length/2 - 1; i > 0; i--) {
+        for (int i = matrix.length / 2 - 1; i > 0; i--) {
             matrix[i][matrix.length - i - 1] = k;
             k++;
         }
-        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
+        for (int i = matrix.length - 1; i > matrix.length / 2; i--) {
             matrix[0][i] = k;
             k++;
         }
-
-        k = 1;
-        for (int i = matrix.length/2; i < matrix.length - 1; i++) {
-            matrix[i][matrix.length/2] = k;
-            k++;
-        }
-        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
-            matrix[i][matrix.length/2 + matrix.length - i - 1] = k;
-            k++;
-        }
-        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
-            matrix[matrix.length/2][i] = k;
-            k++;
+        for (int i = matrix.length/2; i < matrix.length - 2; i++) {
+            for (int j = matrix.length/2; j < matrix.length - 2; j++) {
+                if(matrix[i][j] == 0)
+                    matrix[i][j] = k;
+            }
         }
 
         k = 1;
-        for (int i = matrix.length/2; i < matrix.length - 1; i++) {
+        for (int i = matrix.length / 2; i < matrix.length - 1; i++) {
+            matrix[i][matrix.length / 2] = k;
+            k++;
+        }
+        for (int i = matrix.length - 1; i > matrix.length / 2; i--) {
+            matrix[i][matrix.length / 2 + matrix.length - i - 1] = k;
+            k++;
+        }
+        for (int i = matrix.length - 1; i > matrix.length / 2; i--) {
+            matrix[matrix.length / 2][i] = k;
+            k++;
+        }
+        for (int i = 0; i < matrix.length/2 - 2; i++) {
+            for (int j = matrix.length/2; j < matrix.length - 2; j++) {
+                if(matrix[i][j] == 0)
+                    matrix[i][j] = k;
+            }
+        }
+
+        k = 1;
+        for (int i = matrix.length / 2; i < matrix.length - 1; i++) {
             matrix[i][0] = k;
             k++;
         }
-        for (int i = matrix.length - 1; i > matrix.length/2; i--) {
+        for (int i = matrix.length - 1; i > matrix.length / 2; i--) {
             matrix[i][matrix.length - i - 1] = k;
             k++;
         }
-        for (int i = matrix.length/2 - 1; i > 0; i--) {
-            matrix[matrix.length/2][i] = k;
+        for (int i = matrix.length / 2 - 1; i > 0; i--) {
+            matrix[matrix.length / 2][i] = k;
             k++;
         }
-
+        for (int i = matrix.length/2; i < matrix.length - 2; i++) {
+            for (int j = 0; j < matrix.length/2 - 2; j++) {
+                if(matrix[i][j] == 0)
+                    matrix[i][j] = k;
+            }
+        }
         MatrOutput(matrix);
+
     }
 }
